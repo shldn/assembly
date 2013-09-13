238d9be64680268bb2f907ba94b9cb79b65b6d60
Wes Hawkins <wes.r.hawkins@gmail.com>
2013-09-13 14:38:02 -0700

    Better UI, camera controls, etc.

--- NodeCollective/Assets/Scripts/CameraControl.cs
+++ NodeCollective/Assets/Scripts/CameraControl.cs
@@ -5,48 +5,115 @@ public class CameraControl : MonoBehaviour {
 	
 	public float cameraRotateSpeed = 2.0f;
 	public float cameraMoveSpeed = 2.0f;
-	
+
+    public float orbitRotateSpeed = 2.0f;
+    public float orbitZoomSpeed = 6.0f;
+
+
 	Vector3 targetEulers;
 	float smoothVelEulersX;
 	float smoothVelEulersY;
 	public float rotSmoothTime = 0.1f;
+
+    Vector3 targetPos;
+    Vector3 smoothVelTranslate;
+    public float translateSmoothTime = 0.1f;
 	
-	public Texture2D crosshairTex;
-	
-	public GameObject lookedAtObject;
+	[HideInInspector] public GameObject lookedAtObject;
 
     bool cameraLock;
+
+    bool cameraFocused;
+    Vector3 focusedPos;
+    Node focusedNode;
+    public Texture2D nodeSelectTex;
+
+    Vector2 orbitEulers;
+    float orbitDist = 10.0f;
+
+    bool spacePressed;
 	
 	void Start()
 	{
 		targetEulers = transform.eulerAngles;
 	} // End of Start().
 
-	void Update()
+	void FixedUpdate()
 	{
-        // Smoothly interpolate camera rotation.
+        // Smoothly interpolate camera position/rotation.
+        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref smoothVelTranslate, translateSmoothTime);
+
         Vector3 tempEulers = transform.eulerAngles;
         tempEulers.x = Mathf.SmoothDampAngle(tempEulers.x, targetEulers.x, ref smoothVelEulersX, rotSmoothTime);
         tempEulers.y = Mathf.SmoothDampAngle(tempEulers.y, targetEulers.y, ref smoothVelEulersY, rotSmoothTime);
         transform.eulerAngles = tempEulers;
 
-        if (Input.GetKeyDown(KeyCode.Escape))
+        // Toggle camera lock/free movement with Spacebar.
+        if (Input.GetKey(KeyCode.Space) && !spacePressed)
+        {
+            spacePressed = true;
             cameraLock = !cameraLock;
+        }
+        if (!Input.GetKey(KeyCode.Space))
+            spacePressed = false;
 
+
+        cameraFocused = false;
         if (cameraLock)
         {
             Screen.lockCursor = false;
 
+            // Move in/out with mousewheel.
+            targetPos += Camera.main.transform.forward * Input.GetAxis("Mouse ScrollWheel") * orbitZoomSpeed;
+
+
+            if(focusedNode)
+            {
+                cameraFocused = true;
+                focusedPos = focusedNode.transform.position;
+
+                // Pump calories in/out with up/down arrow keys.
+                if(Input.GetKey(KeyCode.UpArrow))
+                    focusedNode.calories += 10.0f * Time.deltaTime;
+                if(Input.GetKey(KeyCode.DownArrow))
+                    focusedNode.calories -= 10.0f * Time.deltaTime;
+            }
+
+            if(cameraFocused)
+            {
+                Vector2 xyToFocusedPos = Mathw.XYToPos(Camera.main.transform.position, focusedPos);
+                targetEulers.x = xyToFocusedPos.x;
+                targetEulers.y = xyToFocusedPos.y;
+
+                // Zoom orbit with mousewheel.
+                orbitDist -= Input.GetAxis("Mouse ScrollWheel") * orbitZoomSpeed;
+                orbitDist = Mathf.Clamp(orbitDist, 2.5f, Mathf.Infinity);
+
+                // Rotate orbit via directional input.
+                if (Input.GetMouseButton(1))
+                {
+                    orbitEulers.x -= Input.GetAxis("Mouse Y") * orbitRotateSpeed;
+                    orbitEulers.y += Input.GetAxis("Mouse X") * orbitRotateSpeed;
+                }
+                orbitEulers.x = Mathf.Clamp(orbitEulers.x, -90, 90);
+                targetPos = focusedPos - (Quaternion.Euler(orbitEulers.x, orbitEulers.y, 0) * (Vector3.forward * orbitDist));
+            }
+
             // If camera is locked, user can use cursor to manipulate nodes.
-            if (lookedAtObject)
+            if(lookedAtObject)
             {
                 Node lookedAtNode = lookedAtObject.GetComponent<Node>();
                 if (lookedAtNode)
                 {
                     if (Input.GetMouseButton(0))
-                        lookedAtNode.signal += 10.0f * Time.deltaTime;
+                    {
+                        focusedNode = lookedAtNode;
+                        Vector2 xyToNode = Mathw.XYToPos(Camera.main.transform.position, focusedNode.transform.position);
+                        orbitEulers.x = xyToNode.x;
+                        orbitEulers.y = xyToNode.y;
+                    }
                     else if (Input.GetMouseButton(1))
-                        lookedAtNode.signal -= 10.0f * Time.deltaTime;
+                        lookedAtNode.signal = 1.0f;
                 }
             }
         }
@@ -54,6 +121,7 @@ public class CameraControl : MonoBehaviour {
         {
             Screen.lockCursor = true;
             lookedAtObject = null;
+            focusedNode = null;
 
             // Rotate camera via mouse movement.
             targetEulers.x -= Input.GetAxis("Mouse Y") * cameraRotateSpeed;
@@ -61,22 +129,20 @@ public class CameraControl : MonoBehaviour {
             targetEulers.x = Mathf.Clamp(targetEulers.x, -90, 90);
 
             // Translate position with keyboard input.
-            Vector3 tempPosition = transform.position;
             if (Input.GetKey(KeyCode.W))
-                tempPosition += transform.forward * cameraMoveSpeed * Time.deltaTime;
+                targetPos += transform.forward * cameraMoveSpeed * Time.deltaTime;
             if (Input.GetKey(KeyCode.S))
-                tempPosition -= transform.forward * cameraMoveSpeed * Time.deltaTime;
+                targetPos -= transform.forward * cameraMoveSpeed * Time.deltaTime;
 
             if (Input.GetKey(KeyCode.A))
-                tempPosition -= transform.right * cameraMoveSpeed * Time.deltaTime;
+                targetPos -= transform.right * cameraMoveSpeed * Time.deltaTime;
             if (Input.GetKey(KeyCode.D))
-                tempPosition += transform.right * cameraMoveSpeed * Time.deltaTime;
+                targetPos += transform.right * cameraMoveSpeed * Time.deltaTime;
 
-            if (Input.GetKey(KeyCode.Space))
-                tempPosition += transform.up * cameraMoveSpeed * Time.deltaTime;
             if (Input.GetKey(KeyCode.LeftShift))
-                tempPosition -= transform.up * cameraMoveSpeed * Time.deltaTime;
-            transform.position = tempPosition;
+                targetPos += transform.up * cameraMoveSpeed * Time.deltaTime;
+            if (Input.GetKey(KeyCode.LeftControl))
+                targetPos -= transform.up * cameraMoveSpeed * Time.deltaTime;
         }
 		
 		// Determine what the user is looking at.
@@ -86,10 +152,35 @@ public class CameraControl : MonoBehaviour {
 			lookedAtObject = cameraLookHit.transform.gameObject;
 		else
 			lookedAtObject = null;
-
-        
-
-		
-		
 	} // End of Update().
+
+
+    void OnGUI()
+    {
+        // Show camera type/controls.
+        GUI.skin = GameManager.readoutSkin;
+        GUI.skin.label.alignment = TextAnchor.UpperCenter;
+        string viewInfo = "";
+
+        // Camera settings
+        if(!cameraLock)
+            viewInfo += "FREE CAMERA\n(Spacebar to lock)\n\nWASD, Shift, Ctrl to navigate.\nMouse to look around.";
+        if(cameraLock && !cameraFocused)
+            viewInfo += "LOCKED CAMERA\n(Spacebar to unlock)\n\nMousewheel to move in/out.\nClick node to focus.";
+        if (cameraLock && focusedNode)
+            viewInfo += "ORBIT CAMERA\n(Spacebar to unlock)\n\nMousewheel to zoom in/out.\nRight click and drag mouse to rotate.\n\nRight click to generate signal.\nUp/Down arrows to input/siphon calories.\nClick another node to focus.";
+
+        GUI.Label(new Rect(0, 0, Screen.width, Screen.height), viewInfo);
+
+
+        // Indicate selected node.
+        if(focusedNode)
+        {
+            Vector3 selectedNodeScreenPos = Camera.main.WorldToScreenPoint(focusedNode.transform.position);
+            selectedNodeScreenPos.y = Screen.height - selectedNodeScreenPos.y;
+            float selectionBoxWidth = 650.0f / Vector3.Distance(Camera.main.transform.position, focusedNode.transform.position);
+            Rect selectionBoxRect = new Rect(selectedNodeScreenPos.x - (selectionBoxWidth * 0.5f), selectedNodeScreenPos.y - (selectionBoxWidth * 0.5f), selectionBoxWidth, selectionBoxWidth);
+            GUI.Label(selectionBoxRect, nodeSelectTex);
+        }
+    } // End of OnGUI().
 }
---
+++ NodeCollective/Assets/Scripts/Mathw.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mathw
+{
+    public static Vector2 XYToPos(Vector3 start, Vector3 end)
+    {
+        Vector2 returnAngles;
+        returnAngles.y = Mathf.Atan2(end.x - start.x, end.z - start.z) * Mathf.Rad2Deg;
+        returnAngles.x = Mathf.Atan2(end.y - start.y, Vector2.Distance(new Vector2(start.x, start.z), new Vector2(end.x, end.z))) * -Mathf.Rad2Deg;
+        return returnAngles;
+    }
+}
