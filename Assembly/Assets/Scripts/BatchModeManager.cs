f9311cae9f435a7720c421591dc465878fbe0e50
Erik Hill <eghill@ucsd.edu>
2013-11-06 12:05:55 -0800

    BatchModeManager - Bare bones to handle a batch mode client.

---
+++ Assembly/Assets/Scripts/BatchModeManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class BatchModeManager {
+
+    private static BatchModeManager mInstance = null;
+    public static BatchModeManager Inst {
+        get {
+            if (mInstance == null)
+                mInstance = new BatchModeManager();
+            return mInstance;
+        }
+    }
+
+    // Member variables
+    private bool inBatchMode = false;
+
+    // Accessors
+    public bool InBatchMode { get{ return InBatchMode; } }
+
+
+    private BatchModeManager() {
+        HandleCommandLineArgs();
+    }
+
+    private void HandleCommandLineArgs() {
+        string[] cmdLnArgs = System.Environment.GetCommandLineArgs();
+        for (int i = 0; i < cmdLnArgs.Length; i++) {
+            switch (cmdLnArgs[i]) {
+                case "-batchmode":
+                    inBatchMode = true;
+                    break;
+                default:
+                    Debug.LogError("Unknown command line arg: " + cmdLnArgs[i]);
+                    break;
+            }
+        }
+    }
+}
