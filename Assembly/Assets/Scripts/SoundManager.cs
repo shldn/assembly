d85a7bead4d5409f718876c998afeb1957025241
Wes Hawkins <wes.r.hawkins@gmail.com>
2014-01-14 13:25:19 -0800

    Missing sounds and textures from last commit.

---
+++ Assembly/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+
+	public static SoundManager inst = null;
+
+    public AudioClip replicate;
+
+    void Awake(){
+        inst = this;
+    } // End of Awake().
+} // End of SoundManager class.
