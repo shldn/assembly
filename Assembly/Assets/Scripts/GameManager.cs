578fe926f0c236da13e8cbbfb17d5519724e36c9
Kitt <kponkaew@gmail.com>
2014-04-18 13:50:51 -0700

    added control bars. -kitt

--- Assembly/Assets/Scripts/GameManager.cs
+++ Assembly/Assets/Scripts/GameManager.cs
@@ -265,6 +265,25 @@ public class GameManager : MonoBehaviour {
             controlGuiRect.y += guiHeight;
             GUI.enabled = true;
             */
+
+            /*GUI to control food detection and distance*/
+            GUI.Label(controlGuiRect, "Adjust Detection Range: " + Node.detectRange);
+            controlGuiRect.y += guiHeight;
+            Node.detectRange = GUI.HorizontalSlider(controlGuiRect, Node.detectRange, 10F, 100F);
+            controlGuiRect.y += guiHeight;
+            if(Node.detectRange < Node.consumeRange)
+                Node.consumeRange = Node.detectRange;
+            GUI.Label(controlGuiRect, "Adjust Consume Range: " + Node.consumeRange);
+            controlGuiRect.y += guiHeight;
+            Node.consumeRange = GUI.HorizontalSlider(controlGuiRect, Node.consumeRange, 10F, 100F);
+            if( Node.consumeRange > Node.detectRange)
+                Node.detectRange = Node.consumeRange;
+            controlGuiRect.y += guiHeight;
+            GUI.Label(controlGuiRect, "Adjust Consume Rate: " + Node.consumeRate);
+            controlGuiRect.y += guiHeight;
+            Node.consumeRate = GUI.HorizontalSlider(controlGuiRect, Node.consumeRate, 5F, 20F);
+            controlGuiRect.y += guiHeight;
+
         }
 
 
