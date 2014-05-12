34375f4b6bfd7715e728934928bbbdcfd6623d69
Wes <wes.r.hawkins@gmail.com>
2014-05-12 13:26:37 -0700

    Missing files

---
+++ Assembly/Assets/Scripts/Amalgam.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Amalgam : MonoBehaviour {
+
+
+	void Update(){
+	
+        if(Assembly.GetAll().Count > 4)
+            UpdatePoints();
+	} // End of Update().
+
+    void UpdatePoints(){
+
+        // get node positions
+        List<Vector3> amalgamPoints = new List<Vector3>();
+
+        foreach (Assembly assem in Assembly.GetAll()){
+            amalgamPoints.Add(assem.WorldPosition);
+        }
+
+        // apply the convex hull to the mesh
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        ConvexHull.UpdateMeshFromPoints(amalgamPoints, ref mesh);
+
+	} // End of Start().
+} // End of Amalgam.
