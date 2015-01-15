d130178e342abe12db154787b1366d0c3615bfcb
Erik Hill <eghill@ucsd.edu>
2015-01-15 15:38:44 -0800

    Spring Creature - first iteration, basic attaching of springs on mesh faces.

---
+++ Assembly/Assets/Scripts/GroundGame/AttachmentHelpers.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttachmentHelpers{
+
+    public static void AttachSpringToObject(GameObject parent, Vector3 positionOffset, Vector3 attachNormal)
+    {
+        // create spring
+        Object prefabObj = Resources.Load("GroundGame/Spring");
+        GameObject spring = GameObject.Instantiate(prefabObj) as GameObject;
+        spring.transform.position = parent.transform.position + positionOffset;
+        spring.transform.up = attachNormal;
+
+        // attach spring to parent object
+        FixedJoint joint = parent.AddComponent<FixedJoint>();
+        joint.connectedBody = spring.rigidbody;
+    }
+
+    public static Vector3 GetRandomAttachPoint(Mesh m, out Vector3 normal)
+    {
+        int triangleIdx = Random.Range(0, (m.triangles.Length / 3));
+        return GetAttachPoint(m, triangleIdx, out normal);
+    }
+
+    public static Vector3 GetAttachPoint(Mesh m, int triangleIdx, out Vector3 normal)
+    {
+        triangleIdx *= 3;
+        int[] vertIdx = { m.triangles[triangleIdx + 0], m.triangles[triangleIdx + 1], m.triangles[triangleIdx + 2] };
+        normal = Vector3.up;
+        if (m.normals.Length > vertIdx[0])
+            normal = (m.normals[vertIdx[0]] + m.normals[vertIdx[1]] + m.normals[vertIdx[2]]) / 3.0f;
+        return (m.vertices[vertIdx[0]] + m.vertices[vertIdx[1]] + m.vertices[vertIdx[2]]) / 3.0f;
+    }
+
+}
---
+++ Assembly/Assets/Scripts/GroundGame/SpringCreature.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpringCreature : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+        Vector3 normal = Vector3.up;
+        AttachmentHelpers.AttachSpringToObject(gameObject, AttachmentHelpers.GetRandomAttachPoint(gameObject.GetComponent<MeshFilter>().mesh, out normal), normal);
+        AttachmentHelpers.AttachSpringToObject(gameObject, AttachmentHelpers.GetRandomAttachPoint(gameObject.GetComponent<MeshFilter>().mesh, out normal), normal);
+        AttachmentHelpers.AttachSpringToObject(gameObject, AttachmentHelpers.GetRandomAttachPoint(gameObject.GetComponent<MeshFilter>().mesh, out normal), normal);
+        AttachmentHelpers.AttachSpringToObject(gameObject, AttachmentHelpers.GetRandomAttachPoint(gameObject.GetComponent<MeshFilter>().mesh, out normal), normal);
+        AttachmentHelpers.AttachSpringToObject(gameObject, AttachmentHelpers.GetRandomAttachPoint(gameObject.GetComponent<MeshFilter>().mesh, out normal), normal);
+	}
+	
+
+}
