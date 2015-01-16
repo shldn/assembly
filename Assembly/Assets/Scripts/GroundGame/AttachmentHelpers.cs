bfa099160f13495c36b244e472e8d5cc614ac571
Erik Hill <eghill@ucsd.edu>
2015-01-15 16:27:40 -0800

    Started adding variables to the SpringCreature to control its attributes. Spring attachments can have random orientations.

--- Assembly/Assets/Scripts/GroundGame/AttachmentHelpers.cs
+++ Assembly/Assets/Scripts/GroundGame/AttachmentHelpers.cs
@@ -3,25 +3,37 @@ using System.Collections;
 
 public class AttachmentHelpers{
 
-    public static void AttachSpringToObject(GameObject parent, Vector3 positionOffset, Vector3 attachNormal)
+    // Attaches a spring to the parent game object and returns the spring
+    public static GameObject AttachSpringToObject(GameObject parent, Vector3 positionOffset, Vector3 attachNormal)
     {
         // create spring
         Object prefabObj = Resources.Load("GroundGame/Spring");
         GameObject spring = GameObject.Instantiate(prefabObj) as GameObject;
         spring.transform.position = parent.transform.position + positionOffset;
-        spring.transform.up = attachNormal;
+
+        // randomly rotate spring direction vector, stay on proper side of the plane orthogonal to attachNormal.
+        // Random vector on unit hemisphere about y-up vector
+        Vector3 randV = Random.onUnitSphere;
+        randV.y = Mathf.Abs(randV.y);
+
+        Quaternion adjustment = Quaternion.FromToRotation(Vector3.up, attachNormal);
+        spring.transform.up = adjustment * randV;
 
         // attach spring to parent object
         FixedJoint joint = parent.AddComponent<FixedJoint>();
         joint.connectedBody = spring.rigidbody;
+
+        return spring;
     }
 
+    // Chooses a random triangle on the mesh and returns the position of the center of the triangle.
     public static Vector3 GetRandomAttachPoint(Mesh m, out Vector3 normal)
     {
         int triangleIdx = Random.Range(0, (m.triangles.Length / 3));
         return GetAttachPoint(m, triangleIdx, out normal);
     }
 
+    // returns the position of the center of the indicated triangle on mesh m.
     public static Vector3 GetAttachPoint(Mesh m, int triangleIdx, out Vector3 normal)
     {
         triangleIdx *= 3;
--- Assembly/Assets/Scripts/GroundGame/SpringCreature.cs
+++ Assembly/Assets/Scripts/GroundGame/SpringCreature.cs
@@ -3,14 +3,20 @@ using System.Collections;
 
 public class SpringCreature : MonoBehaviour {
 
-	// Use this for initialization
-	void Awake () {
+    public int numSprings = 5;
+    public int repeatDelayMin = 1;
+    public int repeatDelayMax = 6;
+	
+	void Start () {
         Vector3 normal = Vector3.up;
-        AttachmentHelpers.AttachSpringToObject(gameObject, AttachmentHelpers.GetRandomAttachPoint(gameObject.GetComponent<MeshFilter>().mesh, out normal), normal);
-        AttachmentHelpers.AttachSpringToObject(gameObject, AttachmentHelpers.GetRandomAttachPoint(gameObject.GetComponent<MeshFilter>().mesh, out normal), normal);
-        AttachmentHelpers.AttachSpringToObject(gameObject, AttachmentHelpers.GetRandomAttachPoint(gameObject.GetComponent<MeshFilter>().mesh, out normal), normal);
-        AttachmentHelpers.AttachSpringToObject(gameObject, AttachmentHelpers.GetRandomAttachPoint(gameObject.GetComponent<MeshFilter>().mesh, out normal), normal);
-        AttachmentHelpers.AttachSpringToObject(gameObject, AttachmentHelpers.GetRandomAttachPoint(gameObject.GetComponent<MeshFilter>().mesh, out normal), normal);
+        for (int i = 0; i < numSprings; ++i)
+        {
+            GameObject spring = AttachmentHelpers.AttachSpringToObject(gameObject, AttachmentHelpers.GetRandomAttachPoint(gameObject.GetComponent<MeshFilter>().mesh, out normal), normal);
+
+            // set repeat delay
+            BoxColliderStagedScaler scaler = spring.GetComponent<BoxColliderStagedScaler>();
+            scaler.repeatDelay = Random.Range((float)repeatDelayMin, (float)repeatDelayMax);
+        }
 	}
 	
 
