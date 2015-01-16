9ebf661dd70109da8c763ed1c3d307fb5605b18f
Erik Hill <eghill@ucsd.edu>
2015-01-16 09:23:44 -0800

    Added initial game manager for GroundGame. Can add more creatures with a key press.

---
+++ Assembly/Assets/Scripts/GroundGame/GroundGameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundGameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyUp(KeyCode.N))
+        {
+            GameObject cube = GameObject.CreatePrimitive(Input.GetKey(KeyCode.RightShift) ? PrimitiveType.Sphere : PrimitiveType.Cube);
+            cube.AddComponent<Rigidbody>();
+            cube.AddComponent<SpringCreature>();
+            cube.transform.position = new Vector3(0, 5.5F, 0);
+        }
+	}
+}
