614140b148b5ea71a8c6ba9b016ad0bd054a55c6
Erik Hill <eghill@ucsd.edu>
2015-02-04 11:38:47 -0800

    Prefab Packer -- places meshes along a base mesh. Added more junk prefabs to Resources folder.

---
+++ Assembly/Assets/Scripts/GroundGame/PrefabPacker.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PrefabPacker : MonoBehaviour {
+
+    public List<GameObject> prefabs = new List<GameObject>();
+    public Mesh packShape = null;
+
+    public bool alignWithFaces = false;
+    public Vector3 scale = Vector3.one;
+
+    int prefabIdx = 0;
+    Vector3 lastScale = Vector3.one;
+
+    int NextPrefabIdx { get { return prefabIdx++ % prefabs.Count; } }
+
+	void Start () {
+        Pack();
+	}
+
+    void Pack()
+    {
+        if (packShape == null || prefabs.Count == 0)
+            return;
+
+        Matrix4x4 scaleAdjust = Matrix4x4.Scale(scale);
+        Vector3 posAdjust = transform.position;
+        Vector3 normal = Vector3.up;
+        for (int i = 0; i < packShape.triangles.Length / 3; ++i)
+        {
+            Vector3 pos = scaleAdjust * AttachmentHelpers.GetAttachPoint(packShape, i, out normal);
+            GameObject go = GameObject.Instantiate(prefabs[NextPrefabIdx], pos + posAdjust, Quaternion.LookRotation(normal)) as GameObject;
+
+            if( alignWithFaces )
+                go.transform.Rotate(Vector3.right, 90.0f, Space.Self);
+
+            if( go.rigidbody)
+                go.rigidbody.isKinematic = true;
+            go.transform.parent = this.transform;
+        }
+    }
+
+    void UpdatePositions()
+    {
+        Matrix4x4 scaleAdjust = Matrix4x4.Scale(scale);
+        Vector3 posAdjust = transform.position;
+        Vector3 normal = Vector3.up;
+        for (int i = 0; i < gameObject.transform.childCount; ++i)
+            gameObject.transform.GetChild(i).transform.position = (Vector3)(scaleAdjust * AttachmentHelpers.GetAttachPoint(packShape, i, out normal)) + posAdjust;
+    }
+
+    void Drop()
+    {
+        for (int i = 0; i < gameObject.transform.childCount; ++i)
+        {
+            gameObject.transform.GetChild(i).rigidbody.isKinematic = false;
+            gameObject.transform.GetChild(i).rigidbody.WakeUp();
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Alpha0))
+            Drop();
+        if (lastScale != scale)
+            UpdatePositions();
+        lastScale = scale;
+    }
+
+    //void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.white;
+    //    Gizmos.DrawWireSphere(transform.position, 5);
+    //}
+}
