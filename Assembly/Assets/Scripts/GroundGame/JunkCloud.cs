9dbea00db5e17fcc32700573dbe81d5e751839fc
Erik Hill <eghill@ucsd.edu>
2015-02-05 17:05:01 -0800

    Junk Cloud - can attach a component to follow an entity and drop prefabs.

---
+++ Assembly/Assets/Scripts/GroundGame/JunkCloud.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class JunkCloud : MonoBehaviour {
+
+    public List<GameObject> rainPrefabs = new List<GameObject>();
+
+	public Bounds cloudBounds = new Bounds(new Vector3(0,5,0), new Vector3(10,3,10));
+	public float delayBetweenDrops = 1.0f;
+
+    int prefabIdx = 0;
+    int NextPrefabIdx { get { return prefabIdx++ % rainPrefabs.Count; } }
+
+	void Start () {
+        Invoke("Rain", delayBetweenDrops);	
+	}
+
+    void Rain()
+    {
+        GameObject go = GameObject.Instantiate(rainPrefabs[NextPrefabIdx], RandomPointInCloud(), Random.rotation) as GameObject;
+        if (delayBetweenDrops > 0)
+            Invoke("Rain", delayBetweenDrops);
+    }
+
+    Vector3 RandomPointInCloud()
+    {
+        return transform.position + (new Vector3(Random.Range(cloudBounds.min.x, cloudBounds.max.x), Random.Range(cloudBounds.min.y, cloudBounds.max.y), Random.Range(cloudBounds.min.z, cloudBounds.max.z)));
+    }
+
+
+}
