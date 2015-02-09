542baf9dd750fd6502b6d97311ca7b84a1ee7b02
Erik Hill <eghill@ucsd.edu>
2015-02-09 10:26:17 -0800

    Level Transitions added to LevelManager functions.

--- Assembly/Assets/Scripts/LevelManager.cs
+++ Assembly/Assets/Scripts/LevelManager.cs
@@ -4,30 +4,30 @@ public class LevelManager {
 
     public static void LoadNextLevel()
     {
-        Application.LoadLevel((Application.loadedLevel + 1) % Application.levelCount);
+        TransitionManager.Inst.NextLevel();
     }
 
     public static void LoadPrevLevel()
     {
-        Application.LoadLevel((Application.loadedLevel + Application.levelCount - 1) % Application.levelCount);
+        TransitionManager.Inst.PreviousLevel();
     }
 
     public static void LoadLevel(int i)
     {
-        Application.LoadLevel(i);
+        TransitionManager.Inst.ChangeLevel(i);
     }
 
     // Must be called from each level's game manager - or we refactor to have a game manager that is never destroyed across levels.
     public static void InputHandler()
     {
         if (Input.GetKeyUp(KeyCode.Equals))
-            TransitionManager.Inst.NextLevel();
+            LoadNextLevel();
         if (Input.GetKeyUp(KeyCode.Minus))
-            TransitionManager.Inst.PreviousLevel();
+            LoadPrevLevel();
         if (Input.GetKeyUp(KeyCode.Alpha1))
-            TransitionManager.Inst.ChangeLevel(0);
+            LoadLevel(0);
         if (Input.GetKeyUp(KeyCode.Alpha2))
-            TransitionManager.Inst.ChangeLevel(1);
+            LoadLevel(1);
 
     }
 }
