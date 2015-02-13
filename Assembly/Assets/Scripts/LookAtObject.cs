395004164c8c27c1a0f53fabf99d45295912a0ee
Erik Hill <eghill@ucsd.edu>
2015-02-13 13:35:01 -0800

    LookAtObject - set orientation of an object to look at another object - can use for eyes.

---
+++ Assembly/Assets/Scripts/LookAtObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtObject : MonoBehaviour {
+
+    public GameObject lookAtMe = null;
+    public bool useUpDir = false;
+	
+	void Update () {
+        if (lookAtMe != null)
+        {
+            gameObject.transform.LookAt(lookAtMe.transform.position);
+            if( useUpDir )
+                gameObject.transform.Rotate(Vector3.right, 90.0f);
+        }
+	}
+}
