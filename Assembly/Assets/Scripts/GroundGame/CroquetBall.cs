c30b752b731f62afef6a407515d91527d141f1c9
Erik Hill <eghill@ucsd.edu>
2015-03-06 11:38:20 -0800

    Box Man - added croquet ball to hand.
    Former-commit-id: 9d1b29e41bc46e81f1af886625e647758d69df73

---
+++ Assembly/Assets/Scripts/GroundGame/CroquetBall.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CroquetBall : MonoBehaviour {
+
+    public static void Create(Vector3 pos, Quaternion rot)
+    {
+        GameObject go = (GameObject)GameObject.Instantiate(Resources.Load("GroundGame/CroquetBall"));
+        go.transform.position = pos;
+        go.transform.rotation = rot;
+    }
+
+    public static IEnumerator CreateDelayedImpl(Vector3 pos, Quaternion rot, float waitSeconds)
+    {
+        yield return new WaitForSeconds(waitSeconds);
+        Create(pos, rot);
+    }
+
+    void OnMouseUpAsButton()
+    {
+        Transform playerTransform = GroundGameManager.Inst.LocalPlayer.gameObject.transform;
+
+        // put player in position
+        float distanceFromBall = 1.0f;
+        //GameManager.Inst.playerManager.SetLocalPlayerTransform(gameObject.transform.position + (distanceFromBall * -playerTransform.forward.normalized), playerTransform.rotation);
+        //GameManager.Inst.LocalPlayer.gameObject.GetComponent<AnimatorHelper>().StartAnim("Swing", true);
+    }
+}
