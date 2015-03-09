cecbe06a629f9ba814c42b464c4244681569c030
sangel <sangel@ucsd.edu>
2015-03-09 11:37:17 -0700

    Updated the avatar files, included the new canvas material for the ribbon character, and added in the mallet prep animation clip to put the mallet in the correct position when swinging.
    Former-commit-id: dd3c6e5f7d31acab1f077b5d0e435b3694be352c

---
+++ Assembly/Assets/Scripts/BaseMapHack.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseMapHack : MonoBehaviour {
+	
+	void Update () {
+	    if (Terrain.activeTerrain.basemapDistance != 20000){
+             Terrain.activeTerrain.basemapDistance = 20000;
+         }
+	} // End of Update().
+} // End of BaseMapHack.
