ab1d7ab0117978ea41ee72a923d05267a410bec8
Erik Hill <eghill@ucsd.edu>
2015-03-24 12:26:15 -0700

    Unique ID - Added class to grab mac address or android id from a client.
    Former-commit-id: 6dbd7ef134b1fdd9eceb2fe0f3885f065c1d4575

---
+++ Assembly/Assets/Scripts/Unique.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net.NetworkInformation;
+
+public class Unique {
+
+    public static string ID {
+        get{
+#if UNITY_STANDALONE
+            return GetMacAddress();
+#elif UNITY_ANDRIOD
+            return GetAndroidID();
+#endif
+        }
+    }
+
+    private static string GetMacAddress()
+    {
+        IPGlobalProperties computerProperties = IPGlobalProperties.GetIPGlobalProperties();
+        NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+
+        if (nics == null || nics.Length < 1)
+        {
+            Debug.LogError("No network interfaces found.");
+            return "";
+        }
+
+        foreach (NetworkInterface adapter in nics)
+        {
+            IPInterfaceProperties properties = adapter.GetIPProperties();
+            PhysicalAddress address = adapter.GetPhysicalAddress();
+            byte[] bytes = address.GetAddressBytes();
+            return address.ToString();
+        }
+        return "";
+    }
+
+    private static string GetAndroidID()
+    {
+        AndroidJavaClass up = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject currentActivity = up.GetStatic<AndroidJavaObject>("currentActivity");
+        AndroidJavaObject contentResolver = currentActivity.Call<AndroidJavaObject>("getContentResolver");
+        AndroidJavaClass secure = new AndroidJavaClass("android.provider.Settings$Secure");
+        return secure.CallStatic<string>("getString", contentResolver, "android_id");
+    }
+
+}
