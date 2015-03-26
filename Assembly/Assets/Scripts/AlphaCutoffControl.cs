3788278e3888e2f61e7e3ef7ba2e30773d398fbf
sangel <sangel@ucsd.edu>
2015-03-26 11:38:30 -0700

    Chain man now works as a playable character. He is just missing some animations that I will be working on to get him as up to speed with the other characters.
    I also wrote a script that controls the Alpha cutoff value of the clouds and animates it with ping pong math. It currently sits at value to make sure the environment isn't crazy wacky.
    
    Lastly I deleted shaders I created but never ended up using.
    
    
    Former-commit-id: 584a46fe10d51dc6eb686dc6bcab38b51472dc4b

---
+++ Assembly/Assets/Scripts/AlphaCutoffControl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlphaCutoffControl : MonoBehaviour 
+{
+
+	Shader alphaControls;
+	public Renderer rend;
+	
+	// Grab the renderer
+	void Start () 
+	{
+		//Set the renderer to a variable for ease of typing		
+        rend = GetComponent<Renderer>();
+		
+		//Grab the Alpha Cutoff Value from the shader		
+        rend.material.shader = Shader.Find("Transparent/Cutout/Soft Edge Unlit");
+    }
+	
+	//	On each update animate the alpha cutoff value to determin the cloudiness of the sky
+    void Update() {
+		
+		//Assign animating math to cloudiness variable as well as a variable to control how it pingpongs.
+        var smoothTime = 0.3;
+		float cloudiness = Mathf.PingPong(0.2f, 1.0f);
+		
+		//Set cloudiness variable to Shader's cutoff value
+        rend.material.SetFloat("_Cutoff", cloudiness);
+    }
+}
+
