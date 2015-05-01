6384b33a4cf46f9111e811aa87919129d5efc192
Erik Hill <eghill@ucsd.edu>
2015-05-01 11:01:13 -0700

    Turned admin client build back on by default.
    Former-commit-id: 71a31c6377d9371925729fc876196abdb83fef46

--- Assembly/Assets/Scripts/PersistentGameManager.cs
+++ Assembly/Assets/Scripts/PersistentGameManager.cs
@@ -16,7 +16,7 @@ public class PersistentGameManager : MonoBehaviour {
         }
     }
 
-    public static bool IsAdminClient { get { return false; } }
+    public static bool IsAdminClient { get { return true; } }
     public static bool IsClient { get { return Application.loadedLevelName == "CaptureClient"; } }
     public static bool IsServer { get { return !IsClient; } }
 
