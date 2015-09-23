d846ea6aa264c7a7d94f629f34061ced296b8f22
Erik Hill <eghill@ucsd.edu>
2015-09-22 22:41:02 -0700

    NeuroScale Demo - Optimized Cull technique to use octree and some special cases.  -- Have not culled Food yet.  -- Think there should be another layer, after a certain point, cull on an Assembly basis instead of a Node basis.  -- For a single Assembly, cull less frequently so use doesn't see Nodes popping in and out as much.
    Former-commit-id: ff9b5fcdd44e98699a4cfda015e1b5fab964c30f

--- Assembly/Assets/Scripts/NeuroScaleDemo.cs
+++ Assembly/Assets/Scripts/NeuroScaleDemo.cs
@@ -1,20 +1,30 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class NeuroScaleDemo : MonoBehaviour {
 
 	public static NeuroScaleDemo Inst;
 
 	// How much information is shown to the user, from 0f (very little) to 1f (everything)
-	float enviroScale = 0f;
+	public float enviroScale = 0f;
 
 	// As this increases, the number of nodes shown will increase--chosen radiating out from the origin node.
 	int numNodesToShow = 1;
 	int numFoodToShow = 0;
+    int lastNumNodesToShow = 1;
 
 	//ThreeAxisCreep simulator;
 
+    bool newSelectedNode = false;
 
+    // Octree optimization -- Perhaps use a sortedList for the first 50 nodes, then just let the octree cull whole assemblies    
+    Bounds nodeCullBoundary = new Bounds();
+    SortedList<float, Node> nodeSortedSet = new SortedList<float, Node>();
+
+
+    // Testing
+    public bool useOctree = true;
+    bool lastUseOctree = false;
 
 	void Awake(){
 		Inst = this;
@@ -32,9 +42,11 @@ public class NeuroScaleDemo : MonoBehaviour {
 	} // End of Start().
 	
 	void Update(){
-		
+
+        newSelectedNode = false;
 		if((!CameraControl.Inst.selectedNode || CameraControl.Inst.selectedNode.cull) && (Node.getAll.Count > 0)){
 			CameraControl.Inst.selectedNode = Node.getAll[Random.Range(0, Node.getAll.Count)];
+            newSelectedNode = true;
 			print("Getting new node to focus.");
 		}
 
@@ -45,32 +57,115 @@ public class NeuroScaleDemo : MonoBehaviour {
 		RenderSettings.fogStartDistance = CameraControl.Inst.targetRadius + (1000f * Mathf.Pow(enviroScale, 2f));
 		RenderSettings.fogEndDistance = RenderSettings.fogStartDistance * 2f;
 
+        lastNumNodesToShow = numNodesToShow;
 		numNodesToShow = 1 + Mathf.RoundToInt(Mathf.Pow(enviroScale, 3f) * Node.getAll.Count);
 		numFoodToShow = Mathf.RoundToInt(Mathf.Pow(enviroScale, 2f) * FoodPellet.all.Count);
 
-
-		SortNodes();
-		SortFood();
-		for(int i = 0; i < Node.getAll.Count; i++){
-			if(Node.getAll[i].cubeTransform)
-				Node.getAll[i].cubeTransform.renderer.enabled = i < numNodesToShow;
-		}
-		for(int i = 0; i < FoodPellet.all.Count; i++){
-			FoodPellet.all[i].render = i < numNodesToShow;
-		}
+        Cull();
 
 		if(Input.GetKey(KeyCode.UpArrow))
 			enviroScale += Time.deltaTime * 0.2f;
 		if(Input.GetKey(KeyCode.DownArrow))
 			enviroScale -= Time.deltaTime * 0.2f;
 
+
 		//enviroScale = Mathf.Pow(0.5f + simulator.creep.x, 2f);
 
 		CameraControl.Inst.targetOrbit.x -= Time.deltaTime * 5f;
 
 		enviroScale = Mathf.Clamp01(enviroScale);
+
+        lastUseOctree = useOctree;
+
 	} // End of Update().
 
+
+    void Cull()
+    {
+        if (useOctree)
+        {
+            nodeSortedSet.Clear();
+
+            // Only worry about the first assembly if the node count is low enough.
+            if (CameraControl.Inst.selectedNode.PhysAssembly.NodeDict.Count >= numNodesToShow)
+            {
+                // Only cull when the enviroScale amount changes to minimize the popping in and out of nodes in the same assembly as one gets further from the selectedNode
+                if (newSelectedNode || lastNumNodesToShow != numNodesToShow || !lastUseOctree)
+                {
+                    CullNodes(CameraControl.Inst.selectedNode.PhysAssembly);
+                    HandleCulledVisibility();
+                }
+            }
+            else if (enviroScale >= 0.99f || numNodesToShow >= Node.getAll.Count)
+                SetAllNodeVisibility(true);
+            else
+            {
+                float boundsSize = RenderSettings.fogEndDistance - RenderSettings.fogStartDistance;
+                nodeCullBoundary = new Bounds(CameraControl.Inst.selectedNode.Position, boundsSize * Vector3.one);
+                Assembly.AllAssemblyTree.RunActionInRange(new System.Action<Assembly>(CullNodes), nodeCullBoundary);
+                HandleCulledVisibility();
+            }
+
+        }
+        else
+        {
+            // Previous Approach
+            SortNodes();
+            SortFood();
+            for (int i = 0; i < Node.getAll.Count; i++)
+            {
+                if (Node.getAll[i].cubeTransform)
+                    Node.getAll[i].cubeTransform.renderer.enabled = i < numNodesToShow;
+            }
+            for (int i = 0; i < FoodPellet.all.Count; i++)
+            {
+                FoodPellet.all[i].render = i < numNodesToShow;
+            }
+        }
+    } // End of Cull().
+
+    void CullNodes(Assembly someAssembly)
+    {
+        foreach (KeyValuePair<Triplet, Node> node in someAssembly.NodeDict)
+        {
+            if (nodeSortedSet.Count < numNodesToShow)
+            {
+                float dist = (node.Value.Position - CameraControl.Inst.selectedNode.Position).sqrMagnitude;
+                nodeSortedSet.Add(dist, node.Value);
+            }
+            else
+            {
+                float dist = (node.Value.Position - CameraControl.Inst.selectedNode.Position).sqrMagnitude;
+                if (nodeSortedSet.Keys[nodeSortedSet.Keys.Count - 1] > dist)
+                {
+                    nodeSortedSet.RemoveAt(nodeSortedSet.Keys.Count - 1);
+                    nodeSortedSet.Add(dist, node.Value);
+                }
+            }
+        }
+    } // End of CullNodes().
+
+    void SetAllNodeVisibility(bool vis)
+    {
+        for (int i = 0; i < Node.getAll.Count; i++)
+        {
+            if (Node.getAll[i].cubeTransform)
+                Node.getAll[i].cubeTransform.renderer.enabled = vis;
+        }
+    } // End of SetAllNodeVisibility().
+
+    void HandleCulledVisibility()
+    {
+        SetAllNodeVisibility(false);
+
+        // turn on selected nodes
+        foreach (KeyValuePair<float, Node> nodePair in nodeSortedSet)
+        {
+            if (nodePair.Value.cubeTransform)
+                nodePair.Value.cubeTransform.renderer.enabled = true;
+        }
+    } // End of HandleCulledVisibility().
+    
 
 	// bubble-sort nodes
 	void SortNodes(){
