8d20d783fa4c94b88960a324531e055f7a5758d3
Erik Hill <eghill@ucsd.edu>
2015-09-23 17:10:32 -0700

    Muse Integration  - Added Head Connection Status indicators ( 4 ints - 0 = bad, 1 = good )  - Added Battery Percentage Indication.  - Measure if we haven't received a message from the muse for awhile.
    Former-commit-id: 91a4980273df2a1814dbce5e81351f86999fd0e5

--- Assembly/Assets/Scripts/MuseManager.cs
+++ Assembly/Assets/Scripts/MuseManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System;
+using System.Collections.Generic;
 
 public class MuseManager : MonoBehaviour {
 
@@ -8,11 +9,17 @@ public class MuseManager : MonoBehaviour {
     // Local variables
     private bool touchingForehead = false;
     private float lastConcentrationMeasure = 0f;
+    private float batteryLevel = 1.0f;
+    private List<int> headConnectionStatus = new List<int>() {0,0,0,0};
+    private DateTime timeOfLastMessage = DateTime.Now;
 
 
     // Accessors
     public bool TouchingForehead{get{return touchingForehead;}}
     public float LastConcentrationMeasure{get{return lastConcentrationMeasure;}}
+    public float BatteryPercentage { get { return batteryLevel; } }
+    public float SecondsSinceLastMessage { get { return (float)(DateTime.Now - timeOfLastMessage).TotalSeconds; } }
+    public List<int> HeadConnectionStatus { get { return headConnectionStatus; } }
 
 
     void Awake(){
@@ -26,11 +33,14 @@ public class MuseManager : MonoBehaviour {
     
     private void Server_PacketReceivedEvent(UnityOSC.OSCServer sender, UnityOSC.OSCPacket packet)
     {
+        //Debug.Log("packet: " + packet.Address + " " + OSCHandler.DataToString(packet.Data));
         if (packet.Address.Contains("touching_forehead"))
             touchingForehead = ((int)packet.Data[0] != 0);
         else if (packet.Address.Contains("concentration"))
             HandleConcentrationSample((float)packet.Data[0]);
-
+        else if(packet.Address.Contains("is_good"))
+            HandleHeadConnectMessage(packet.Data);
+        timeOfLastMessage = DateTime.Now;
     }
 
     void HandleConcentrationSample(float sample)
@@ -38,10 +48,24 @@ public class MuseManager : MonoBehaviour {
         lastConcentrationMeasure = sample;
     }
 
+    // These are status messages for connection to the user's head.
+    // 0 = bad, 1 = good
+    void HandleHeadConnectMessage(List<object> data)
+    {
+        for (int i = 0; i < headConnectionStatus.Count && i < data.Count; ++i)
+            headConnectionStatus[i] = (int)data[i];
+    }
+
+    void HandleBatteryStatus(List<object> data)
+    {
+        batteryLevel = ((int)data[0]) / 100000f;
+    }
+
     void OnGUI()
     {
         GUI.skin.label.fontSize = 18;
         GUI.skin.label.alignment = TextAnchor.LowerCenter;
-        GUI.Label(new Rect(0.25f * Screen.width, 0.25f * Screen.height, 0.5f * Screen.width, 0.5f * Screen.height), touchingForehead ? "" : "No device detected.");
+        string statusStr = SecondsSinceLastMessage > 1 ? (SecondsSinceLastMessage.ToString() + " secs since last msg") : ""; // headConnectionStatus[0] + " " + headConnectionStatus[1] + " " + headConnectionStatus[2] + " " + headConnectionStatus[3];
+        GUI.Label(new Rect(0.25f * Screen.width, 0.25f * Screen.height, 0.5f * Screen.width, 0.5f * Screen.height), touchingForehead ? statusStr : (statusStr + "\nNo device detected."));
     }
 }
