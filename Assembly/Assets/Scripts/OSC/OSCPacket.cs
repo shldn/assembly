16751c81c3358ab708f8183007d9217958d0c7b8
Erik Hill <eghill@ucsd.edu>
2015-10-06 13:01:52 -0700

    One more OSC source file blocked out for mobile builds.
    Former-commit-id: 98edd3c6461e6fed42598528f96105feac35398d

--- Assembly/Assets/Scripts/OSC/OSCPacket.cs
+++ Assembly/Assets/Scripts/OSC/OSCPacket.cs
@@ -17,7 +17,7 @@
 // 	  CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 // 	  IN THE SOFTWARE.
 //
-
+#if UNITY_STANDALONE
 using System;
 using System.Diagnostics;
 using System.Collections.Generic;
@@ -314,3 +314,4 @@ namespace UnityOSC
 		#endregion
 	}
 }
+#endif
