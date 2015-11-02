4ba7f60f2f0e4c174080158578b17765582f56ee
Wes Hawkins <wes.r.hawkins@gmail.com>
2015-11-02 13:28:37 -0800

    oops
    Former-commit-id: 1f65f6b0a3446fe92ddb6dd5ee7ca63ab040d3bb

---
+++ Assembly/Assets/Scripts/RaidenLaser.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaidenLaser : MonoBehaviour {
+
+	LineRenderer lineRenderer;
+	public int resolution = 10;
+	public Material material = null;
+	public float width = 1f;
+
+	public Transform startTrans;
+	public Transform endTrans;
+
+	float textureRunner = 0f;
+
+
+	void Start(){
+		lineRenderer = gameObject.AddComponent<LineRenderer>();
+		lineRenderer.material = material;
+	} // End of Start().
+	
+
+	void Update(){
+		textureRunner += Time.deltaTime;
+
+		float distBetween = Vector3.Distance(startTrans.position, endTrans.position);
+		int vertexCount = Mathf.CeilToInt(resolution * distBetween + 1);
+		lineRenderer.material.mainTextureScale = new Vector2(distBetween * 0.5f, 1f);
+		lineRenderer.material.mainTextureOffset = new Vector2(textureRunner, 0f);
+		lineRenderer.SetVertexCount(vertexCount);
+		lineRenderer.SetWidth(width, width);
+		for(int i = 0; i < (vertexCount + 1); i++)
+			lineRenderer.SetPosition(i, MathUtilities.CalculateBezierPoint((float)i / (float)vertexCount, startTrans.position, startTrans.position + (startTrans.forward * distBetween * 0.5f), endTrans.position - (endTrans.position - startTrans.position).normalized, endTrans.position));
+	} // End of Update().
+} // End of RaidenLaser.
