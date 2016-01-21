795cdee0a29e1c9eae95bc345798c451aefc22ef
Erik Hill <eghill@ucsd.edu>
2016-01-21 13:18:42 -0800

    Editor script to copy the config file to the export directory.
    Former-commit-id: 3ccd01b0d2a19011468f18b4555bd83d360f089f

---
+++ Assembly/Assets/Editor/CopyConfig.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+public class CopyConfig : MonoBehaviour {
+    [PostProcessBuild]
+    public static void OnPostprocessBuild(BuildTarget target, string pathToBuildProject) {
+        if(target == BuildTarget.StandaloneWindows || target == BuildTarget.StandaloneWindows64) {
+            int fileStartIdx = pathToBuildProject.LastIndexOf('/');
+            string path = pathToBuildProject.Substring(0, fileStartIdx + 1);
+            Debug.Log("Build Post Process - copying config file to " + path);
+            FileUtil.ReplaceFile("config.txt", path + "config.txt");
+        }
+    }
+}
