95642b3489637351a2d19328b90f4592d1f4cd3e
Erik Hill <eghill@ucsd.edu>
2016-05-06 09:18:35 -0700

    Updated IcoSphere Test Script.
    Former-commit-id: d3ec1f4f27d3428c7391227f2483acac1f4659c6

--- Assembly/Assets/Scripts/Test/TestIcoInteractive.cs
+++ Assembly/Assets/Scripts/Test/TestIcoInteractive.cs
@@ -5,7 +5,8 @@ public class TestIcoInteractive : MonoBehaviour {
 
 	void Update () {
         bool inside = false;
-        IcoSphereCreator.Inst.GetProjectedFace(gameObject.transform.position, out inside);
+        IcoSphereCreator.Inst.GetProjectedFace(gameObject.transform.position / 80f, null, out inside);
+        //inside = CognoAmalgam.Inst.IsInside(gameObject.transform.position);
         GetComponent<Renderer>().material.color = inside ? Color.green : Color.red;
 	}
 }
