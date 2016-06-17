11499ed3f8a004efaf2834d08a65126a6fdaf32b
Erik Hill <eghill@ucsd.edu>
2016-06-17 14:50:57 -0700

    Turn off hand representations when they are out of view or not yet networked. -- Needs some more testing.
    Former-commit-id: 61d8f754a4a6c954a93e02a9e272d10aa3638573

--- Assembly/Assets/Scripts/SmoothNetPosition.cs
+++ Assembly/Assets/Scripts/SmoothNetPosition.cs
@@ -7,7 +7,19 @@ public class SmoothNetPosition : MonoBehaviour {
 	public static List<SmoothNetPosition> allFingertips = new List<SmoothNetPosition>();
 	int id = 0;
 	Vector3 targetPosition = Vector3.zero;
-	public bool render = false;
+    bool render = true;
+	public bool Render {
+        get { return render; }
+        set {
+            if(value != render) {
+                render = value;
+                GetComponent<Renderer>().enabled = value;
+                Transform child = transform.GetChild(0);
+                if (child != null)
+                    child.gameObject.SetActive(value);
+            }
+        }
+    }
 
 	public NetworkView netView;
 	Vector3 posVel = Vector3.zero;
@@ -17,6 +29,8 @@ public class SmoothNetPosition : MonoBehaviour {
 
 		id = allFingertips.Count;
 		allFingertips.Add(this);
+
+        Render = false;
 
 		if(id < 10)
 			transform.localScale = Vector3.one * 100f;
@@ -29,8 +43,12 @@ public class SmoothNetPosition : MonoBehaviour {
 		else
 			transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref posVel, 0.2f);
 
-		//GetComponent<Renderer>().enabled = render;
-		render = false;
+		GetComponent<Renderer>().enabled = Render;
+        Transform child = transform.GetChild(0);
+        if (!Render && child != null && child != null)
+            child.gameObject.SetActive(Render);
+
+		Render = false;
 
 	} // End of Update().
 
@@ -43,7 +61,7 @@ public class SmoothNetPosition : MonoBehaviour {
 		// When sending my own data out...
 		if(stream.isWriting){
 			transferPos = targetPosition;
-			netRender = render;
+			netRender = Render;
 
 			stream.Serialize(ref transferPos);
 			stream.Serialize(ref netRender);
@@ -54,7 +72,7 @@ public class SmoothNetPosition : MonoBehaviour {
 			stream.Serialize(ref netRender);
 
 			targetPosition = transferPos;
-			render = netRender;
+			Render = netRender;
 		}
     } // End of OnSerializeNetworkView().
 
