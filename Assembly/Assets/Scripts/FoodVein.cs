28f1587b59f9a63d0433822de4d67f51b22010a7
Erik Hill <eghill@ucsd.edu>
2017-05-19 14:47:58 -0700

    FoodVein initial implementation.
    Former-commit-id: d982940fa656f0854400966cb0e864ad581420ab

---
+++ Assembly/Assets/Scripts/FoodVein.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodVein : MonoBehaviour {
+
+    List<Transform> food = new List<Transform>();
+    int length = 200;
+    int numBranches = 2;
+    float spacing = 2f;
+    float branchAngle = 30f;
+
+    float decayRate = 0.5f;
+
+	void Start () {
+        BuildVein();
+    }
+
+    void BuildVein() {
+        int branchesBuilt = 0;
+        for (int i = 0; i < length; ++i) {
+            Transform f = ViewerController.Inst.FoodPool.Get().transform;
+            f.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+            f.position = transform.position + i * spacing * transform.forward.normalized;
+            f.forward = transform.forward;
+            f.parent = transform;
+            if(i > 0 && i % (length / (numBranches + 1)) == 0 && length > 1) {
+                float sign = ++branchesBuilt % 2 == 1 ? 1f : -1f;
+                f.forward = Quaternion.AngleAxis(sign * branchAngle, transform.up) * f.forward;
+                FoodVein vein = f.gameObject.AddComponent<FoodVein>();
+                vein.length = (int)(decayRate * length);
+            }
+
+            food.Add(f);
+        }
+    }
+}
