afcb7183e7a541fff1ecb90fcbe47356be9ab600
Erik Hill <eghill@ucsd.edu>
2017-05-19 15:59:28 -0700

    First iteration of spherical coordinates
    Former-commit-id: 9e373ddb9cd4fb28c8c2aeef937bf93f5a4cc970

--- Assembly/Assets/Scripts/FoodVein.cs
+++ Assembly/Assets/Scripts/FoodVein.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 public class FoodVein : MonoBehaviour {
 
     List<Transform> food = new List<Transform>();
-    int length = 200;
-    int numBranches = 2;
+    int length = 50;
+    int numBranches = 1;
     float spacing = 2f;
-    float branchAngle = 30f;
+    float branchAngle = 80f;
 
     float decayRate = 0.5f;
 
 	void Start () {
-        BuildVein();
+        //BuildVein();
+        BuildSphericalVein();
     }
 
     void BuildVein() {
@@ -34,4 +35,28 @@ public class FoodVein : MonoBehaviour {
             food.Add(f);
         }
     }
+    void BuildSphericalVein() {
+        int branchesBuilt = 0;
+        SphericalCoordinates currentSpherePos = new SphericalCoordinates(transform,1,200, 0, 2*Mathf.PI, 0, 2*Mathf.PI);
+        for (int i = 0; i < length; ++i) {
+            Transform f = ViewerController.Inst.FoodPool.Get().transform;
+            f.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+            SphericalCoordinates newSpherePos = new SphericalCoordinates(transform, 1, 200, 0, 2 * Mathf.PI, 0, 2 * Mathf.PI);
+            //newSpherePos.SetRotation(currentSpherePos.polar, currentSpherePos.elevation + i * spacing * Mathf.Deg2Rad);
+            newSpherePos.SetRotation(currentSpherePos.polar + i * spacing * transform.forward.x * Mathf.Deg2Rad, currentSpherePos.elevation + i * spacing * transform.forward.y * Mathf.Deg2Rad);
+            f.position = newSpherePos.toCartesian;
+            f.forward = new Vector3(newSpherePos.polar, newSpherePos.elevation, 0);
+            f.parent = transform;
+            if (i > 0 && i % (length / (numBranches + 1)) == 0 && length > 1) {
+                float sign = ++branchesBuilt % 2 == 1 ? 1f : -1f;
+                f.forward = Quaternion.AngleAxis(sign * branchAngle, f.position.normalized) * f.forward;
+                FoodVein vein = f.gameObject.AddComponent<FoodVein>();
+                vein.length = (int)(decayRate * length);
+            }
+
+            food.Add(f);
+        }
+    }
+
+
 }
