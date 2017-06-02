87e8e4bf48edd23f254e17925c785b1a9db8c43a
Erik Hill <eghill@ucsd.edu>
2017-06-02 10:42:44 -0700

    FoodVein - counter balance in direction to each parent branch of a new branch added.  New branches are also relatively smaller than the parent branch, but not by too much at this point.
    Former-commit-id: 706f47ac90c4d26c9839c67685b283df27e34831

--- Assembly/Assets/Scripts/FoodVein.cs
+++ Assembly/Assets/Scripts/FoodVein.cs
@@ -8,7 +8,7 @@ public class FoodVein : MonoBehaviour {
     int length = 50;
     int numBranches = 2;
     float spacing = 2f;
-    float branchAngle = 30f;
+    float branchAngle = 45f;
     float foodScale = 2f;
 
     float branchLengthDecayRate = 0.5f;
@@ -21,22 +21,40 @@ public class FoodVein : MonoBehaviour {
 
     void BuildVein() {
         int branchesBuilt = 0;
+        Vector3 lastFoodPos = transform.position;
+        Vector3 lastFoodDir = transform.forward;
         for (int i = 0; i < length; ++i) {
             Transform f = ViewerController.Inst.FoodPool.Get().transform;
             f.gameObject.GetComponent<Rigidbody>().isKinematic = true;
-            f.position = transform.position + i * spacing * transform.forward.normalized;
-            f.forward = transform.forward;
+            f.position = lastFoodPos + spacing * lastFoodDir.normalized;
+            f.forward = lastFoodDir;
             f.localScale = foodScale * Vector3.one;
             f.parent = transform;
             // numBranches + 1 and ignore the end caps
             if(i > 0 && (i < length - 2) && i % (length / (numBranches + 1)) == 0 && length > 1) {
                 float sign = ++branchesBuilt % 2 == 1 ? 1f : -1f;
-                f.forward = Quaternion.AngleAxis(sign * branchAngle, transform.up) * f.forward;
+                f.forward = Quaternion.AngleAxis(sign * branchAngle, transform.up) * lastFoodDir;
                 FoodVein vein = f.gameObject.AddComponent<FoodVein>();
                 vein.length = (int)(branchLengthDecayRate * length);
-                vein.foodScale = foodScale;
-            }
 
+                float branchPercent = 0.9f;
+                vein.foodScale = branchPercent * foodScale;
+
+                // size of branch should be a proportion of the parent branch and the parent should lose size proportionally
+                //float branchPercent = 0.25f;
+                //vein.foodScale = branchPercent * foodScale;
+                //// main branch reduces its size be branch size
+                //foodScale = (1f - branchPercent) * foodScale;
+
+                // adjust for the branch, by offsetting the parent branch angle a bit
+                float parentAngle = 0.2f * branchAngle;
+                lastFoodDir = Quaternion.AngleAxis(-sign * parentAngle, transform.up) * lastFoodDir;
+            }
+            else
+                lastFoodDir = f.forward;
+
+
+            lastFoodPos = f.position;
             food.Add(f);
             foodScale = foodSizeDecayRate * foodScale;
         }
