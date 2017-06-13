85542168adc5863cfdb7aae2644b16dd555e583e
Erik Hill <eghill@ucsd.edu>
2017-06-13 10:13:11 -0700

    Added 3D noise and made 3D Spherical Noisy vein.
    Former-commit-id: 7191939c83743a42a5e83cf0ea3bc780796b693b

--- Assembly/Assets/Scripts/FoodVein.cs
+++ Assembly/Assets/Scripts/FoodVein.cs
@@ -10,30 +10,37 @@ using UnityEngine;
 public class FoodVein : MonoBehaviour {
 
     List<Transform> food = new List<Transform>();
-    int length = 50;
+    int length = 150;
     int numBranches = 3;
     float spacing = 2f;
     float branchAngleMin = 15f;
     float branchAngleMax = 55f;
-    float foodScale = 2f;
+    float foodScale = 3f;
 
+    float minFoodScale = 0.05f;
     float foodSizeDecayRate = 0.98f;
     float branchLengthDecayRate = 0.5f; // works well for noisy veins (not as good for trees)
 
+    NoiseTest.OpenSimplexNoise noise3d;
+
     void Start () {
+        noise3d = new NoiseTest.OpenSimplexNoise();
+
         //BuildVein();
-        BuildNoiseVein();
+        //BuildNoiseVein();
         //BuildSphericalVein();
+        BuildNoisySphericalVein();
     }
 
     void BuildVein() {
+        if (foodScale < minFoodScale)
+            return;
         int branchesBuilt = 0;
         Vector3 lastFoodPos = transform.position;
         Vector3 lastFoodDir = transform.forward;
         float startBranchSign = Random.value > 0.5f ? 1f : -1f;
         for (int i = 0; i < length; ++i) {
-            Transform f = ViewerController.Inst.FoodPool.Get().transform;
-            f.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+            Transform f = GetNewFood();
             f.position = lastFoodPos + spacing * lastFoodDir.normalized;
             f.forward = lastFoodDir;
             f.localScale = foodScale * Vector3.one;
@@ -70,17 +77,26 @@ public class FoodVein : MonoBehaviour {
         }
     }
 
-    Vector3 GetNextBestNoisePos(Vector3 lastPos, Vector3 dir) {
+    Transform GetNewFood() {
+        Transform f = ViewerController.Inst.FoodPool.Get().transform;
+        f.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+        f.gameObject.GetComponent<MeshRenderer>().enabled = false;
+        return f;
+    }
+
+    // This is 2D noise and assumes y is up, need alternate implementation for 3D noise.
+    Vector3 GetNextBest2DNoisePos(Vector3 lastPos, Vector3 dir) {
+        Vector3 up = transform.up;
         // test possible points around the lastPos for the best choice according to a noise function
         List<Vector3> testPts = new List<Vector3>() { lastPos + spacing * dir,
-                                                      lastPos + spacing * (Quaternion.AngleAxis(60f, transform.up) * dir),
-                                                      lastPos + spacing * (Quaternion.AngleAxis(45f, transform.up) * dir),
-                                                      lastPos + spacing * (Quaternion.AngleAxis(30f, transform.up) * dir),
-                                                      lastPos + spacing * (Quaternion.AngleAxis(15f, transform.up) * dir),
-                                                      lastPos + spacing * (Quaternion.AngleAxis(-15f, transform.up) * dir),
-                                                      lastPos + spacing * (Quaternion.AngleAxis(-30f, transform.up) * dir),
-                                                      lastPos + spacing * (Quaternion.AngleAxis(-45f, transform.up) * dir),
-                                                      lastPos + spacing * (Quaternion.AngleAxis(-60f, transform.up) * dir)
+                                                      lastPos + spacing * (Quaternion.AngleAxis(60f, up) * dir),
+                                                      lastPos + spacing * (Quaternion.AngleAxis(45f, up) * dir),
+                                                      lastPos + spacing * (Quaternion.AngleAxis(30f, up) * dir),
+                                                      lastPos + spacing * (Quaternion.AngleAxis(15f, up) * dir),
+                                                      lastPos + spacing * (Quaternion.AngleAxis(-15f, up) * dir),
+                                                      lastPos + spacing * (Quaternion.AngleAxis(-30f, up) * dir),
+                                                      lastPos + spacing * (Quaternion.AngleAxis(-45f, up) * dir),
+                                                      lastPos + spacing * (Quaternion.AngleAxis(-60f, up) * dir)
         };
         float bestNoiseVal = -1f;
         Vector3 bestPos = testPts[0];
@@ -94,16 +110,40 @@ public class FoodVein : MonoBehaviour {
         return bestPos;
     }
 
+    Vector3 GetNextBest3DNoisePos(Vector3 lastPos, Vector3 dir, Vector3 up) {
+        // test possible points around the lastPos for the best choice according to a noise function
+        List<Vector3> testPts = new List<Vector3>() { lastPos + spacing * dir,
+                                                      lastPos + spacing * (Quaternion.AngleAxis(60f, up) * dir),
+                                                      lastPos + spacing * (Quaternion.AngleAxis(45f, up) * dir),
+                                                      lastPos + spacing * (Quaternion.AngleAxis(30f, up) * dir),
+                                                      lastPos + spacing * (Quaternion.AngleAxis(15f, up) * dir),
+                                                      lastPos + spacing * (Quaternion.AngleAxis(-15f, up) * dir),
+                                                      lastPos + spacing * (Quaternion.AngleAxis(-30f, up) * dir),
+                                                      lastPos + spacing * (Quaternion.AngleAxis(-45f, up) * dir),
+                                                      lastPos + spacing * (Quaternion.AngleAxis(-60f, up) * dir)
+        };
+        double bestNoiseVal = -1f;
+        Vector3 bestPos = testPts[0];
+        for (int i = 0; i < testPts.Count; ++i) {
+            double noise = noise3d.Evaluate(testPts[i].x, testPts[i].y, testPts[i].z);
+            if (noise > bestNoiseVal) {
+                bestNoiseVal = noise;
+                bestPos = testPts[i];
+            }
+        }
+        return bestPos;
+    }
+
     void BuildNoiseVein() {
+        if (foodScale < minFoodScale)
+            return;
         int branchesBuilt = 0;
         Vector3 lastFoodPos = transform.position;
         Vector3 lastFoodDir = transform.forward;
         float startBranchSign = Random.value > 0.5f ? 1f : -1f;
         for (int i = 0; i < length; ++i) {
-            Transform f = ViewerController.Inst.FoodPool.Get().transform;
-            f.gameObject.GetComponent<Rigidbody>().isKinematic = true;
-            
-            f.position = GetNextBestNoisePos(lastFoodPos, lastFoodDir.normalized);
+            Transform f = GetNewFood();
+            f.position = GetNextBest2DNoisePos(lastFoodPos, lastFoodDir.normalized);
             f.forward = lastFoodDir;
             f.localScale = foodScale * Vector3.one;
             f.parent = transform;
@@ -138,7 +178,10 @@ public class FoodVein : MonoBehaviour {
             foodScale = foodSizeDecayRate * foodScale;
         }
     }
+
     void BuildSphericalVein() {
+        if (foodScale < minFoodScale)
+            return;
         int branchesBuilt = 0;
         float distFromCenter = transform.position.magnitude;
         Vector3 lastFoodPos = transform.position;
@@ -146,8 +189,7 @@ public class FoodVein : MonoBehaviour {
         Vector3 lastFoodUp = transform.up;
         float startBranchSign = Random.value > 0.5f ? 1f : -1f;
         for (int i = 0; i < length; ++i) {
-            Transform f = ViewerController.Inst.FoodPool.Get().transform;
-            f.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+            Transform f = GetNewFood();
             f.position = lastFoodPos + spacing * lastFoodDir.normalized;
             Vector3 vToPt = f.position;
             f.position = distFromCenter * vToPt.normalized;
@@ -176,6 +218,52 @@ public class FoodVein : MonoBehaviour {
             }
             lastFoodPos = f.position;
             
+            food.Add(f);
+            foodScale = foodSizeDecayRate * foodScale;
+        }
+    }
+
+
+    void BuildNoisySphericalVein() {
+        if (foodScale < minFoodScale)
+            return;
+        int branchesBuilt = 0;
+        float distFromCenter = transform.position.magnitude;
+        Vector3 lastFoodPos = transform.position;
+        Vector3 lastFoodDir = transform.forward;
+        Vector3 lastFoodUp = transform.up;
+        float startBranchSign = Random.value > 0.5f ? 1f : -1f;
+        for (int i = 0; i < length; ++i) {
+            Transform f = GetNewFood();
+            f.position = lastFoodPos + spacing * lastFoodDir.normalized;
+            f.position = GetNextBest3DNoisePos(lastFoodPos, lastFoodDir.normalized, lastFoodPos.normalized);
+            Vector3 vToPt = f.position;
+            f.position = distFromCenter * vToPt.normalized;
+            Vector3 newForward = Vector3.Cross(lastFoodUp, -f.position.normalized);
+            f.forward = newForward;
+            f.localScale = foodScale * Vector3.one;
+            f.parent = transform;
+            if (i > 0 && (i < length - 2) && i % (length / (numBranches + 1)) == 0 && length > 1) {
+                float sign = ++branchesBuilt % 2 == 1 ? startBranchSign : -startBranchSign;
+                float branchAngle = Random.Range(branchAngleMin, branchAngleMax);
+                f.forward = Quaternion.AngleAxis(sign * branchAngle, f.position.normalized) * newForward;
+                FoodVein vein = f.gameObject.AddComponent<FoodVein>();
+                vein.length = length - i; //(int)(branchLengthDecayRate * length);
+
+                float branchPercent = 0.9f;
+                vein.foodScale = branchPercent * foodScale;
+
+                // adjust for the branch, by offsetting the parent branch angle a bit
+                float parentAngle = 0.2f * branchAngle;
+                lastFoodDir = Quaternion.AngleAxis(-sign * parentAngle, f.position.normalized) * newForward;
+                lastFoodUp = Quaternion.AngleAxis(-sign * parentAngle, f.position.normalized) * lastFoodUp;
+            }
+            else {
+                lastFoodDir = f.forward;
+                lastFoodUp = f.up;
+            }
+            lastFoodPos = f.position;
+
             food.Add(f);
             foodScale = foodSizeDecayRate * foodScale;
         }
